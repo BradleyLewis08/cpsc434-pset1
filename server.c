0c6c297e26d6523f0e27bc034af1f9cbd07f666d
AlexShin <alex.shin@yale.edu>
2023-10-20 22:44:07 -0400

    implementing request parsing

--- server.c
+++ server.c
@@ -7,8 +7,50 @@
 
 #define PORT 8080
 
+void parse_request(char *buffer)
+{
+	//Use strtok_r to tokenize the request --> strtok_r is thread safe (strtok is not)
+
+	char *saveptr;
+	printf("Request: %s\n", buffer);
+	//Toeknize the request line
+	char *token = strtok_r(buffer, " ", &saveptr);
+	char *method = token;
+	token = strtok_r(NULL, " ", &saveptr);
+	char *path = token;
+	token = strtok_r(NULL, " ", &saveptr);
+	char *version = token;
+
+	//Print the request line
+	printf("Method: %s\n", method);
+	printf("Path: %s\n", path);
+	printf("Version: %s\n", version);
+
+	//Tokenize the headers
+	token = strtok_r(NULL, "\r\n", &saveptr);
+	while (token != NULL)
+	{
+		printf("Header: %s\n", token);
+		token = strtok_r(NULL, "\r\n", &saveptr);
+	}
+
+	//Tokenize the body
+	token = strtok_r(NULL, "\r\n", &saveptr);
+	while (token != NULL)
+	{
+		printf("Body: %s\n", token);
+		token = strtok_r(NULL, "\r\n", &saveptr);
+	}
+
+	//Print the body
+	printf("Body: %s\n", token);
+}
+
 void handle_request(char *buffer, int client_fd)
 {
+	// Parse the request
+	parse_request(buffer);
+
 	char *response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
 
 	write(client_fd, response, strlen(response));
